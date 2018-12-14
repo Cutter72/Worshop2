8fe5aedc508c9b721b03a40f9f85fd3853fbd3bb
Cutter72 <drelich_pawel@o2.pl>
2018-12-14 14:12:15 +0100

    Create: public static Connection getConnection

---
+++ CodingSchool/Main1.java
@@ -0,0 +1,17 @@
+package CodingSchool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static Connection getConnection(String dbUserName, String dbPassword, String dbName) throws SQLException {
+        String connString = "jdbc:mysql//localhost:3306/" + dbName + "?useSSL=false";
+        Connection conn = DriverManager.getConnection(connString,dbUserName,dbPassword);
+        return conn;
+    }
+}
